7b26ea02fe7e5904103297b1711cf9d14ac97ddf
ankaankina <freetowninred@gmail.com>
2017-03-29 12:34:36 +0300

    fixed the problem with saving data on the server zzz

--- insert.php
+++ insert.php
@@ -1,19 +1,22 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "php_mvc";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php_mvc";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $sql = "INSERT INTO posts (author, email, content)
-            VALUES ('$_POST[author]', '$_POST[email]', '$_POST[content]')";
+$author = $_POST[author];
+$email = $_POST[email];
+$content = $_POST[content];
+$insText= htmlentities($content, ENT_QUOTES | ENT_HTML5);
+$sql="INSERT INTO posts (author, email, content)
+    VALUES ('$author', '$email', '$insText')";
 
-    if($conn && $sql){
-        $conn->query($sql);
-        printf("Data Submitted succesfully");
-    }
+if($conn && $sql){
+    $conn->query($sql);
+    printf("Data Submitted successfully");
+}
 
-    $conn->close();
+$conn->close();
 ?>
